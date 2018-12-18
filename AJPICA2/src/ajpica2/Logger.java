3ebaf56b236584deb9b468f21b6fb2dd8eb07fbc
Joe Edwards <superturtle24@hotmail.co.uk>
2018-12-18 14:59:11 +0000

    Potential Solution for Concurrancy issue we had at Review

--- AJPICA2/src/ajpica2/Logger.java
+++ AJPICA2/src/ajpica2/Logger.java
@@ -1,5 +1,6 @@
 
 package ajpica2;
+import ajpica2.Ticket.TicketBuilder;
 import java.util.Scanner;
 /**
  * @author Joe
@@ -17,15 +18,26 @@ public class Logger {
         TicketLogger tl = new TicketLogger(loggerHandle, loggerIp, loggerPort);
         tl.begin();
         
+        String ticketHandle = "TicketBuilder";
+        String ticketRecievedIp = "127.0.0.1";
+        int ticketPort = 1000;
+        
+        TicketBuilder simulateTicket = new TicketBuilder(ticketHandle, ticketRecievedIp, ticketPort);
+        simulateTicket.begin();
+        simulateTicket.connectTo(loggerIp, loggerPort);
+        
         while(true)
         {
             Scanner scan = new Scanner(System.in);
             String input = scan.nextLine();
-            System.out.println("Choose an option \n1. Show All Tickets");
+            System.out.println("Choose an option \n1. Show All Tickets  \n2. Show Customers");
             switch(input)
             {
                 case "1":
                 showAllTickets(tl);
+                break;
+                case "2":
+                System.out.println("Not implemented yet");
                 break;
                 default:
                 System.err.println("Not a valid option");
